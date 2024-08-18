68cc0b29dc20e4de631d69db5b673faf4e0e4862
Jordan Quarles <jq@Jordans-Air.lan>
2024-08-18 19:19:07 -0400

    Implement request logging middleware for improved debugging

--- server.js
+++ server.js
@@ -35,6 +35,11 @@ app.use((err, req, res, next) => {
   });
 });
 
+app.use((req, res, next) => {
+  console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
+  next();
+});
+
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   max: 100,
